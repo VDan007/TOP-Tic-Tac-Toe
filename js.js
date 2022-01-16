732804280e2568c5fde787e1b2beeaa02e14a509
Dániel Varjaskéri <varjaskerid@gmail.com>
2022-01-16 22:56:45 +0100

    game logic in place

--- js.js
+++ js.js
@@ -3,10 +3,69 @@ function howsTheGame (){
 const area1 = document.querySelector("#area1");
 const area2 = document.querySelector("#area2");
 const area3 = document.querySelector("#area3");
+const area4 = document.querySelector("#area4");
+const area5 = document.querySelector("#area5");
+const area6 = document.querySelector("#area6");
+const area7 = document.querySelector("#area7");
+const area8 = document.querySelector("#area8");
+const area9 = document.querySelector("#area9");
 
+(function firstRC(){
+    if(area1.textContent&&area2.textContent&&area3.textContent){
+        if(area1.textContent=="X"&&area2.textContent=="X"&&area3.textContent=="X"){console.log("yess X wins");
+       }else if (area1.textContent=="O"&&area2.textContent=="O"&&area3.textContent=="O"){console.log("noo O wins");}
+    }
+})();
 
-//if(area1.firstChild.textContent == "X"&&
-   if(area1.textContent){console.log("j==");}
+(function secondRC(){
+    if(area4.textContent&&area5.textContent&&area6.textContent){
+        if(area4.textContent=="X"&&area5.textContent=="X"&&area6.textContent=="X"){console.log("yess X wins");
+       }else if (area4.textContent=="O"&&area5.textContent=="O"&&area6.textContent=="O"){console.log("noo O wins");}
+    }
+})();
+
+(function thirdRC(){
+    if(area7.textContent&&area8.textContent&&area9.textContent){
+        if(area7.textContent=="X"&&area8.textContent=="X"&&area9.textContent=="X"){console.log("yess X wins");
+       }else if (area7.textContent=="O"&&area8.textContent=="O"&&area9.textContent=="O"){console.log("noo O wins");}
+    }
+})();
+
+(function firstCC(){
+    if(area1.textContent&&area4.textContent&&area7.textContent){
+        if(area1.textContent=="X"&&area4.textContent=="X"&&area7.textContent=="X"){console.log("yess X wins");
+       }else if (area1.textContent=="O"&&area4.textContent=="O"&&area7.textContent=="O"){console.log("noo O wins");}
+    }
+})();
+
+(function secondCC(){
+    if(area2.textContent&&area5.textContent&&area8.textContent){
+        if(area2.textContent=="X"&&area5.textContent=="X"&&area8.textContent=="X"){console.log("yess X wins");
+       }else if (area2.textContent=="O"&&area5.textContent=="O"&&area8.textContent=="O"){console.log("noo O wins");}
+    }
+})();
+
+(function thirdCC(){
+    if(area3.textContent&&area6.textContent&&area9.textContent){
+        if(area3.textContent=="X"&&area6.textContent=="X"&&area9.textContent=="X"){console.log("yess X wins");
+       }else if (area3.textContent=="O"&&area6.textContent=="O"&&area9.textContent=="O"){console.log("noo O wins");}
+    }
+})();
+
+(function xrow1C(){
+    if(area1.textContent&&area5.textContent&&area9.textContent){
+        if(area1.textContent=="X"&&area5.textContent=="X"&&area9.textContent=="X"){console.log("yess X wins");
+       }else if (area1.textContent=="O"&&area5.textContent=="O"&&area9.textContent=="O"){console.log("noo O wins");}
+    }
+})();
+
+(function xrow2C(){
+    if(area3.textContent&&area5.textContent&&area7.textContent){
+        if(area3.textContent=="X"&&area5.textContent=="X"&&area7.textContent=="X"){console.log("yess X wins");
+       }else if (area3.textContent=="O"&&area5.textContent=="O"&&area7.textContent=="O"){console.log("noo O wins");}
+    }
+})();
+
 
 };
 
@@ -14,7 +73,7 @@ const area3 = document.querySelector("#area3");
 function addPlayerMark(){
     let wichPlayer = 0;
     let whoIsNext = Array.from(document.querySelectorAll(".count"));
-    console.log(whoIsNext);
+    
     if(whoIsNext.length % 2 == 0){wichPlayer=0;
     }else{wichPlayer = 1;}
     const x = document.createElement("h1");
@@ -29,7 +88,7 @@ function addPlayerMark(){
     this.appendChild(x);
     this.removeEventListener("click",addPlayerMark);
     this.classList.add("clicked");
-    
+    howsTheGame();
 }
 
 
